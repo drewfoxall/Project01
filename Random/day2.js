3914cf72e736632807c0df8ace66dcd01b0c7517
Drew <andrewfoxal2001@gmail.com>
2022-10-04 20:45:35 -0400

    day2

---
+++ Random/day2.js
@@ -0,0 +1,24 @@
+// string = Basically a sentence, alphabetical variable : "Hello", "Goodbye"
+// number = a basic integer 1,7,10,39,21
+// boolean =  A True or False value False
+// null = A null, devoid of value Null
+// bigint = a wrapper object , appending n to the end of an integer const alsoHuge =
+///BigInt(9007199254740991)
+// undefined = lack of any value, invalid = Undefined
+// symbol = can define symbols and private them for internal use "."
+
+// 3 Objects
+
+const car = { brand: "Honda", car: "Civic", Year: "2019" };
+console.log();
+
+const game = { genre: "RPG", publisher: "Riot Games", Age: "10+" };
+console.log(game.genre);
+
+const alcohol = {};
+alcohol.brand = "Smirnoff";
+alcohol.type = "Vodka";
+alcohol.percentage = 40;
+alcohol.isStrong = True;
+
+console.log(alcohol);
